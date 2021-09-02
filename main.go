4c82fd10c750f71cd573ebd7c93bbaf12921031c
Boris Maslovskiy <maslovskiy@gmail.com>
2021-09-02 19:10:26 +0300

    service 3

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 	e := echo.New()
 	e.Validator = validator.NewCustomValidator()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, this is a Cats service 2!")
+		return c.String(http.StatusOK, "Hello, this is a Cats service 3!")
 	})
 
 	// JWT
